ed66196b409157d248819b2b37ea20854ba7c764
JUMA1725 <juanma182002@outlook.com>
2025-02-10 01:10:36 -0500

    Subiendo todos los archivos

---
+++ formulario.py/formulario.py
@@ -0,0 +1,14 @@
+# listas
+
+# <ol></ol>
+# listas ordenadas, estan enumeradas
+
+# <ul></ul>
+# listas desordenadas, estan con punto
+
+# <div></div>
+# es un contenedor, se usa para crear seciones o agrupar secciones o <span>
+
+# <span></span>
+# solo es una seccion
+<tr> fgsfgsfghs sffdgdffhdg</tr
